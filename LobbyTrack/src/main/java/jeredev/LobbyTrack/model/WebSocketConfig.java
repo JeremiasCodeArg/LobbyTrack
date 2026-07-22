702b14c13660ba4250ae91f805983a0c2bf055de
Jeremias Leiva <jeremiasleiva318@gmail.com>
2026-07-22 13:48:17 -0300

    Commit inicial: Spring Boot/webSocket config

---
+++ LobbyTrack/src/main/java/jeredev/LobbyTrack/model/WebSocketConfig.java
@@ -0,0 +1,34 @@
+package jeredev.LobbyTrack.model;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry){
+        registry.addEndpoint("/ws-audio")
+                .setAllowedOriginPatterns("*");
+
+    }
+
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config){
+        // BROKER
+
+        //Le dice al broker quetodo que comience con /topic es resposonsabilidad suya
+        config.enableSimpleBroker("/topic");
+
+        //le dice al broker, si llega mensaje desde un celular y su destino empieza con /app
+        //no lo repartas, este mensaje es para mi. Frenalo y busca en mis clases controller.
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+}
---
+++ LobbyTrack/src/test/java/jeredev/LobbyTrack/LobbyTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package jeredev.LobbyTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LobbyTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
